440ee69129aa1bf1024b7a9a5003eb8df0a0de55
ljyockey <linyockey@gmail.com>
2017-09-10 16:57:08 -0700

    Initial Commit

---
+++ SwansonQuotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwansonQuotes
+//
+//  Created by L.J. Yockey on 9/10/17.
+//  Copyright © 2017 Yockey Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
