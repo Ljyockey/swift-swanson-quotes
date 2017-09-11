523da6b6a83a26317334cb7ae94cd2a20caf47a9
ljyockey <linyockey@gmail.com>
2017-09-10 19:19:36 -0700

    initial commit

--- SwansonQuotes/ViewController.swift
+++ SwansonQuotes/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var qContainer: UILabel!
+    
+    @IBAction func getQuote(_ sender: Any) {
+        
+        let targetUrl = URL(string: "http://ron-swanson-quotes.herokuapp.com/v2/quotes")
+        let task = URLSession.shared.dataTask(with: targetUrl!) { (data, response, error) in
+            if error != nil {
+                print("ERROR!")
+            }
+            else {
+                if let content = data {
+                    do {
+                        let q = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
+                        let quote = (q[0] as! String)
+                        self.qContainer.text = quote
+                    }
+                    catch {
+                        print("ERROR.")
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
